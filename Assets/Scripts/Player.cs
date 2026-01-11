9af16bb8cdb611a0404fa22b14d8d6aa1ed81660
PeiyiXiong <pxiong2@uci.edu>
2026-01-10 22:10:03 -0800

    1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,30 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
     }
 
     private void Update()
     {
-        
+       float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 moveDirection = new Vector3(horizontal, vertical, 0f).normalized;
+        _playerTransform.Translate(moveDirection * _speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantSeed();
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        if (_numSeedsLeft > 0 && _plantPrefab != null && _plantCountUI != null)
+        {
+            Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+            _numSeedsLeft--;
+            _numSeedsPlanted++;
+            _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+        }
     }
 }
